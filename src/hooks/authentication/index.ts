15ddabaf0c72ca99abc97c1f0578d815f8dbda06
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-10-28 02:32:35 +0530

    error 2

--- src/hooks/authentication/index.ts
+++ src/hooks/authentication/index.ts
@@ -190,36 +190,35 @@ export const useAuthSignUp = () => {
     }
 }
 
-
 export const useGoogleAuth = () => {
     const { signIn, isLoaded: LoadedSignIn } = useSignIn()
     const { signUp, isLoaded: LoadedSignUp } = useSignUp()
-  
+
     const signInWith = (strategy: OAuthStrategy) => {
-      if (!LoadedSignIn) return
-      try {
-        return signIn.authenticateWithRedirect({
-          strategy,
-          redirectUrl: "/callback",
-          redirectUrlComplete: "/callback/sign-in",
-        })
-      } catch (error) {
-        console.error(error)
-      }
+        if (!LoadedSignIn) return
+        try {
+            return signIn.authenticateWithRedirect({
+                strategy,
+                redirectUrl: "/callback",
+                redirectUrlComplete: "/callback/sign-in",
+            })
+        } catch (error) {
+            console.error(error)
+        }
     }
-  
+
     const signUpWith = (strategy: OAuthStrategy) => {
-      if (!LoadedSignUp) return
-      try {
-        return signUp.authenticateWithRedirect({
-          strategy,
-          redirectUrl: "/callback",
-          redirectUrlComplete: "/callback/complete",
-        })
-      } catch (error) {
-        console.error(error)
-      }
+        if (!LoadedSignUp) return
+        try {
+            return signUp.authenticateWithRedirect({
+                strategy,
+                redirectUrl: "/callback",
+                redirectUrlComplete: "/callback/complete",
+            })
+        } catch (error) {
+            console.error(error)
+        }
     }
-  
+
     return { signUpWith, signInWith }
-  }
+}
