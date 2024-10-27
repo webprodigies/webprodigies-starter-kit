fbaf559bee42997fbf39f1cf0314b32628bdc1d2
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-10-27 18:43:38 +0530

    refactor: clean up SVG icon components and remove unnecessary whitespace

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -5,37 +5,39 @@ import { MenuIcon } from "lucide-react"
 import Link from "next/link"
 import Menu from "./menus"
 
-
 type Props = {}
 
 const LandingPageNavbar = (props: Props) => {
-  return (
-    <div className="w-full flex justify-between sticky top-0 items-center py-5 z-50">
-      <p className="font-bold text-2xl">Grouple.</p>
-      <Menu orientation="desktop" />
-      <div className="flex gap-2">
-        <Link href="/sign-in">
-          <Button
-            variant="outline"
-            className="bg-themeBlack rounded-2xl flex gap-2 border-themeGray hover:bg-themeGray"
-          >
-            <Logout />
-            Login
-          </Button>
-        </Link>
-        <GlassSheet
-          triggerClass="lg:hidden"
-          trigger={
-            <Button variant="ghost" className="hover:bg-transparent">
-              <MenuIcon size={30} />
-            </Button>
-          }
-        >
-          <Menu orientation="mobile" />
-        </GlassSheet>
-      </div>
-    </div>
-  )
+    return (
+        <div className="w-full flex justify-between sticky top-0 items-center py-5 z-50">
+            <p className="font-bold text-2xl">Grouple.</p>
+            <Menu orientation="desktop" />
+            <div className="flex gap-2">
+                <Link href="/sign-in">
+                    <Button
+                        variant="outline"
+                        className="bg-themeBlack rounded-2xl flex gap-2 border-themeGray hover:bg-themeGray"
+                    >
+                        <Logout />
+                        Login
+                    </Button>
+                </Link>
+                <GlassSheet
+                    triggerClass="lg:hidden"
+                    trigger={
+                        <Button
+                            variant="ghost"
+                            className="hover:bg-transparent"
+                        >
+                            <MenuIcon size={30} />
+                        </Button>
+                    }
+                >
+                    <Menu orientation="mobile" />
+                </GlassSheet>
+            </div>
+        </div>
+    )
 }
 
 export default LandingPageNavbar
--- src/constants/menus.tsx
+++ src/constants/menus.tsx
@@ -12,138 +12,138 @@ import {
     PersonalDevelopment,
     ZapDouToneBlack,
 } from "@/icons"
-  
-  export type MenuProps = {
+
+export type MenuProps = {
     id: number
     label: string
     icon: JSX.Element
     path: string
     section?: boolean
     integration?: boolean
-  }
-  
-  export type GroupMenuProps = {
+}
+
+export type GroupMenuProps = {
     id: number
     label: string
     icon: JSX.Element
     path: string
-  }
-  
-  export const LANDING_PAGE_MENU: MenuProps[] = [
+}
+
+export const LANDING_PAGE_MENU: MenuProps[] = [
     {
-      id: 0,
-      label: "Home",
-      icon: <Home />,
-      path: "/",
-      section: true,
+        id: 0,
+        label: "Home",
+        icon: <Home />,
+        path: "/",
+        section: true,
     },
     {
-      id: 1,
-      label: "Pricing",
-      icon: <CreditCard />,
-      path: "#pricing",
-      section: true,
+        id: 1,
+        label: "Pricing",
+        icon: <CreditCard />,
+        path: "#pricing",
+        section: true,
     },
     {
-      id: 1,
-      label: "Explore",
-      icon: <Explore />,
-      path: "/explore",
+        id: 1,
+        label: "Explore",
+        icon: <Explore />,
+        path: "/explore",
     },
-  ]
-  export const GROUP_PAGE_MENU: MenuProps[] = [
+]
+export const GROUP_PAGE_MENU: MenuProps[] = [
     {
-      id: 0,
-      label: "Group",
-      icon: <Home />,
-      path: "/",
-      section: true,
-    },
-    {
-      id: 1,
-      label: "Courses",
-      icon: <Courses />,
-      path: "#pricing",
-      section: true,
+        id: 0,
+        label: "Group",
+        icon: <Home />,
+        path: "/",
+        section: true,
     },
     {
-      id: 2,
-      label: "Events",
-      icon: <Buisness />,
-      path: "/explore",
+        id: 1,
+        label: "Courses",
+        icon: <Courses />,
+        path: "#pricing",
+        section: true,
     },
     {
-      id: 3,
-      label: "Members",
-      icon: <PersonalDevelopment />,
-      path: "/explore",
+        id: 2,
+        label: "Events",
+        icon: <Buisness />,
+        path: "/explore",
     },
     {
-      id: 4,
-      label: "About",
-      icon: <Document />,
-      path: "/explore",
+        id: 3,
+        label: "Members",
+        icon: <PersonalDevelopment />,
+        path: "/explore",
     },
     {
-      id: 5,
-      label: "Huddle",
-      icon: <Chat />,
-      path: "/explore",
-    },
-  ]
-  
-  export const SIDEBAR_SETTINGS_MENU: MenuProps[] = [
-    {
-      id: 0,
-      label: "General",
-      icon: <IDuotoneBlack />,
-      path: "",
+        id: 4,
+        label: "About",
+        icon: <Document />,
+        path: "/explore",
     },
     {
-      id: 1,
-      label: "Subscriptions",
-      icon: <CreditCard />,
-      path: "subscriptions",
+        id: 5,
+        label: "Huddle",
+        icon: <Chat />,
+        path: "/explore",
+    },
+]
+
+export const SIDEBAR_SETTINGS_MENU: MenuProps[] = [
+    {
+        id: 0,
+        label: "General",
+        icon: <IDuotoneBlack />,
+        path: "",
     },
     {
-      id: 2,
-      label: "Affiliates",
-      icon: <AffiliateDuoToneBlack />,
-      path: "affiliates",
+        id: 1,
+        label: "Subscriptions",
+        icon: <CreditCard />,
+        path: "subscriptions",
     },
     {
-      id: 3,
-      label: "Domain Config",
-      icon: <GlobeDuoToneBlack />,
-      path: "domains",
+        id: 2,
+        label: "Affiliates",
+        icon: <AffiliateDuoToneBlack />,
+        path: "affiliates",
     },
     {
-      id: 4,
-      label: "Integration",
-      icon: <ZapDouToneBlack />,
-      path: "integrations",
-      integration: true,
+        id: 3,
+        label: "Domain Config",
+        icon: <GlobeDuoToneBlack />,
+        path: "domains",
     },
-  ]
-  
-  type IntegrationsListItemProps = {
+    {
+        id: 4,
+        label: "Integration",
+        icon: <ZapDouToneBlack />,
+        path: "integrations",
+        integration: true,
+    },
+]
+
+type IntegrationsListItemProps = {
     id: string
     name: "stripe"
     logo: string
     description: string
     title: string
     modalDescription: string
-  }
-  
-  export const INTEGRATION_LIST_ITEMS: IntegrationsListItemProps[] = [
+}
+
+export const INTEGRATION_LIST_ITEMS: IntegrationsListItemProps[] = [
     {
-      id: "1",
-      name: "stripe",
-      description:
-        "Stripe is the fastest and easiest way to integrate payments and financial services into your software platform or marketplace.",
-      logo: "914be637-39bf-47e6-bb81-37b553163945",
-      title: "Connect Stripe Account",
-      modalDescription:
-        "The world’s most successful platforms and marketplaces including Shopify and DoorDash, use Stripe Connect.",
+        id: "1",
+        name: "stripe",
+        description:
+            "Stripe is the fastest and easiest way to integrate payments and financial services into your software platform or marketplace.",
+        logo: "914be637-39bf-47e6-bb81-37b553163945",
+        title: "Connect Stripe Account",
+        modalDescription:
+            "The world’s most successful platforms and marketplaces including Shopify and DoorDash, use Stripe Connect.",
     },
-  ]
+]
