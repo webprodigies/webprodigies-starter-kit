9bcee6d5b5a61c139c85e5590a8807fd834b2130
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-11-01 01:44:40 +0530

    Refactor payment actions and group creation page for improved readability and consistency

--- src/actions/payment.ts
+++ src/actions/payment.ts
@@ -1,12 +1,10 @@
-
 "use server"
-import Stripe from 'stripe';
+import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     typescript: true,
     apiVersion: "2024-09-30.acacia",
-});
-
+})
 
 export const onGetStripeClientSecret = async () => {
     try {
@@ -16,15 +14,15 @@ export const onGetStripeClientSecret = async () => {
             automatic_payment_methods: {
                 enabled: true,
             },
-        });
+        })
 
         if (paymentIntent) {
-            return { secret: paymentIntent.client_secret };
+            return { secret: paymentIntent.client_secret }
         }
     } catch (error) {
-        return { status: 400, message: "Failed to load form" };
+        return { status: 400, message: "Failed to load form" }
     }
-};
+}
 
 export const onTransferCommission = async (destination: string) => {
     try {
@@ -32,12 +30,12 @@ export const onTransferCommission = async (destination: string) => {
             amount: 3960,
             currency: "usd",
             destination: destination,
-        });
+        })
 
         if (transfer) {
-            return { status: 200 };
+            return { status: 200 }
         }
     } catch (error) {
-        return { status: 400 };
+        return { status: 400 }
     }
 }
--- src/components/forms/Create-Group/schema.ts
+++ src/components/forms/Create-Group/schema.ts
@@ -1,10 +1,7 @@
-import { z } from "zod";
+import { z } from "zod"
 
 export const CreateGroupSchema = z.object({
-    name: z
-        .string()
-        .min(3, {message: "Name must be at least 3 characters"}),
-    
-        
-    category: z.string().min(3, {message: "You must select a category"}),
+    name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+
+    category: z.string().min(3, { message: "You must select a category" }),
 })
