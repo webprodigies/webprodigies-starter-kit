14d30535c4daa4a97364925416adbbcc908ffa13
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-11-08 00:13:44 +0530

    Add HtmlParser component and integrate it into BlockTextEditor; enhance rich text editor with slash commands and image upload functionality

--- src/components/global/rich-text-editor/index.tsx
+++ src/components/global/rich-text-editor/index.tsx
@@ -1,6 +1,10 @@
-import { JSONContent } from "novel"
+"use client"
+import { cn } from "@/lib/utils"
+import { EditorContent, EditorRoot, JSONContent } from "novel"
+import { handleCommandNavigation } from "novel/extensions"
 import { useState } from "react"
 import { FieldErrors } from "react-hook-form"
+import { HtmlParser } from "../html-parser"
 
 type Props = {
   content: JSONContent | undefined
@@ -39,7 +43,63 @@ const BlockTextEditor = ({
   const [characters, setCharacters] = useState<number | undefined>(
     textContent?.length || undefined,
   )
-  return <div>BlockTextEditor</div>
+  return (
+    <div>
+      {htmlContent && !onEdit && inline ? (
+        <HtmlParser html={htmlContent} />
+      ) : (
+        <EditorRoot>
+          <EditorContent
+            className={cn(
+              inline
+                ? onEdit && "mb-5"
+                : "border-[1px] rounded-xl px-10 py-5 text-base border-themeGray bg-themeBlack w-full",
+            )}
+            initialContent={content}
+            editorProps={{
+              editable: () => !disabled as boolean,
+              handleDOMEvents: {
+                keydown: (_view, event) => handleCommandNavigation(event),
+              },
+              attributes: {
+                class: `prose prose-lg dark:prose-invert focus:outline-none max-w-full [&_h1]:text-4xl [&_h2]:text-3xl [&_h3]:text-2xl text-themeTextGray`,
+              },
+            }}
+            extensions={[
+              // @ts-ignore
+              ...defaultExtensions,
+              // @ts-ignore
+              slashCommand,
+              // @ts-ignore
+              CharacterCount.configure({
+                limit: max,
+              }),
+              // @ts-ignore
+              Placeholder.configure({
+                placeholder: "Type / to insert element...",
+              }),
+              // @ts-ignore
+              Video,
+              // @ts-ignore
+              Image,
+            ]}
+            onUpdate={({ editor }) => {
+              const json = editor.getJSON()
+              const text = editor.getText()
+
+              if (setHtmlContent) {
+                const html = editor.getHTML()
+                setHtmlContent(html)
+              }
+              setContent(json)
+              setTextContent(text)
+              setCharacters(text.length)
+            }}
+          />
+        </EditorRoot>
+      )}
+    </div>
+  )
 }
 
 export default BlockTextEditor
